3bf862147956622ff1f5c0051f229eaaa5103273
MatthewMeyers <jedimastermeyers@gmail.com>
2018-12-11 00:43:44 -0700

    Simplified locator script

--- MayaScripting/scripts/Locator.py
+++ MayaScripting/scripts/Locator.py
@@ -20,31 +20,13 @@ class Locator(Window.Window):
 
         cmds.showWindow(self.mWin)
 
-    def FindMin(self, sels, direction):
-        ''' gets the minimum transform value from a list of objects, requires specific direction: x=0, y=1, z=2 '''
-        minValue = cmds.xform(sels[0], q=True, t=True, ws=True)[direction]
-        for sel in sels:
-            if cmds.xform(sel, q=True, t=True, ws=True)[direction] <= minValue:
-                minValue = cmds.xform(sel, q=True, t=True, ws=True)[direction]
-        return minValue
-
-    def FindMax(self, sels, direction):
-        ''' gets the maximum transform value from a list of objects, requires specific direction: x=0, y=1, z=2 '''
-        maxValue = cmds.xform(sels[0], q=True, t=True, ws=True)[direction]
-        for sel in sels:
-            if cmds.xform(sel, q=True, t=True, ws=True)[direction] >= maxValue:
-                maxValue = cmds.xform(sel, q=True, t=True, ws=True)[direction]
-        return maxValue
-
     def CreateLoc(self, option=1):
         ''' Creates a locator at the center of selection, or the pivot of each object, defaults to former '''
         sels = cmds.ls(sl=True)
 
         if option == 1:
-            minPivot = [self.FindMin(sels, 0), self.FindMin(sels, 1), self.FindMin(sels, 2)]
-            maxPivot = [self.FindMax(sels, 0), self.FindMax(sels, 1), self.FindMax(sels, 2)]
-
-            pivot = [(minPivot[0] + maxPivot[0]) / 2, (minPivot[1] + maxPivot[1]) / 2, (minPivot[2] + maxPivot[2]) / 2]
+            bbox = cmds.exactWorldBoundingBox(sels)
+            pivot = [(bbox[0] + bbox[3]) / 2, (bbox[1] + bbox[4]) / 2, (bbox[2] + bbox[5]) / 2]
 
             loc = cmds.spaceLocator()[0]
             cmds.xform(loc, t=pivot, ws=True)
